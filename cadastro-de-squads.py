e25e0aed8d76ac7fa761059a5859826a8bdf2b32
joaozinsh <joaogabriel241201@gmail.com>
2021-08-04 14:59:21 -0300

    Criação das classes Colaborador e Dev

--- cadastro-de-squads.py
+++ cadastro-de-squads.py
@@ -11,3 +11,19 @@ class Squad:
 
     def incluir_techlead(self, techlead):
         self.techlead = techlead
+
+    def incluir_dev(self, dev):
+        self.devs.append(dev)
+
+class Colaborador(Pessoa):
+    def __init__(self, nome, fone, squad=None):
+        super().__init__(nome, fone)
+        self.squad = squad
+
+    def incluir_squad(self, squad):
+        self.squad = squad
+
+class Dev(Colaborador):
+    def __init__(self, nome, fone, cargo, squad=None):
+        super().__init__(nome, fone, squad)
+        self.cargo = cargo
