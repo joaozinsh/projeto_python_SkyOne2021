9272c94b875e5a4fa037cbc3678e0a298d6f5e4e
joaozinsh <joaogabriel241201@gmail.com>
2021-08-04 15:43:53 -0300

    Implementação da exibição das squads

--- cadastro-de-squads.py
+++ cadastro-de-squads.py
@@ -3,6 +3,9 @@ class Pessoa:
         self.nome = nome
         self.fone = fone
 
+    def exibir(self):
+        print(f'-> {self.nome} - {self.fone}')
+
 class Squad:
     def __init__(self, nome, techlead=None, devs=None):
         self.nome = nome
@@ -28,6 +31,12 @@ class Dev(Colaborador):
         super().__init__(nome, fone, squad)
         self.cargo = cargo
 
+    def exibir(self):
+        super().exibir()
+        print(f'   Cargo de {self.cargo} na squad {self.squad.nome}\n')
+
+print('\n-==-=-=-=-=-=-=-=-=-=-=-Sky.One Solutions-=-=-=-=-=-=-=-=-=-=-==-')
+print('Bem vindo ao sistema de cadastro de squads!\n')
 
 while True:
     squads = []
@@ -57,3 +66,15 @@ while True:
     option = input('\nDeseja adicionar mais uma squad [S/N]: ')
     if option in 'Nn':
         break
+print('\n-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-')
+
+print('\nSquads criadas:')
+for squad in squads:
+    print(f'\n------------------------------{squad.nome}------------------------------')
+    print(f'Techlead: {squad.techlead.nome}')
+    print('\n-----Devs do squad-----')
+    for dev in squad.devs:
+        dev.exibir()
+    print(f'------------------------------{squad.nome}------------------------------')
+
+print('\n-==-=-=-=-=-=-=-=-=-=-=-Sky.One Solutions-=-=-=-=-=-=-=-=-=-=-==-')
